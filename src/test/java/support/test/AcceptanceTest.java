cb482ca0ef2c390cfe91e71a97c665d7f1e6ffa1
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-02-03 17:15:45 +0900

    중복 코드 제거

--- src/test/java/support/test/AcceptanceTest.java
+++ src/test/java/support/test/AcceptanceTest.java
@@ -7,10 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import codesquad.domain.User;
 import codesquad.domain.UserRepository;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -53,5 +58,19 @@ public abstract class AcceptanceTest {
 
     protected Question findByQuestionId(long questionId) {
         return questionRepository.findOne(questionId);
+    }
+
+    protected String createResource(String path, Object bodyPayload) {
+        ResponseEntity<String> response = template().postForEntity(path, bodyPayload, String.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.CREATED));
+        return response.getHeaders().getLocation().getPath();
+    }
+
+    protected <T> T getResource(String location, Class<T> responseType, User loginUser) {
+        return basicAuthTemplate(loginUser).getForObject(location, responseType);
+    }
+
+    protected ResponseEntity<String> getResource(String location, User loginUser) {
+        return basicAuthTemplate(loginUser).getForEntity(location, String.class);
     }
 }
